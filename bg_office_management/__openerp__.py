d749cbafef33b0f823cc23a6a314e2938054ea95
Abhirami <abhiramisbabuhiworth@gmail.com>
2022-12-07 16:33:56 +0530

    new module added bg office management

---
+++ bg_office_management/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'BG Office Management',
+    'description': 'office management forms,tender,surveys etc',
+    'author': 'hiworth',
+    'depends': [
+
+        'hiworth_project_management'
+    ],
+    'data': [
+
+        'views/overhead_costing_commercial.xml',
+        'views/project_overhead_distribution.xml',
+        'views/recurring_expense.xml',
+        'views/project_costing_progress_commercial.xml',
+        'views/boq_estimated_by_qs.xml',
+        'views/boq_excess_order_by_qs.xml',
+        'views/work_hours_planned_by_qs.xml',
+
+    ],
+    'installable': True
+}
